91a0ace855a809b41e29efbe0320c3f4440309b1
iclal1987 <iclalgocmez@gmail.com>
2020-11-05 07:11:06 -0500

    added superman class

---
+++ src/main/java/SuperMan.java
@@ -0,0 +1,5 @@
+public class SuperMan {
+    public static void main(String[] args) {
+        System.out.println("superman");
+    }
+}
---
+++ src/test/java/BatMan.java
@@ -0,0 +1,5 @@
+public class BatMan {
+    public static void main(String[] args) {
+        System.out.println("Batman");
+    }
+}
